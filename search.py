fef16b6c3b4d9613aba50e2a48e00cbc71bd9d75
Fatima Qarni <fatima.q832@gmail.com>
2019-01-11 14:24:32 -0600

    cleanup

--- search.py
+++ search.py
@@ -1,11 +1,15 @@
-import json, os, requests
-
-import helper
+import json, requests
 
 from elasticsearch import Elasticsearch
 
+import helper
 
 def upload_json():
+    """
+    Removes previous json files from elastic search
+    Gets list of all current jsons and uploads them to elastic search
+    """
+
     es = Elasticsearch()
     res = requests.get('http://localhost:9200')
 
@@ -23,6 +27,11 @@ def upload_json():
 
 
 def search():
+    """
+    Allows a search for one search term and displays the number of search
+    results, as well as a list of all files which contain the search term
+    """
+
     es = Elasticsearch()
 
     search_term = input("Enter search term: ")
@@ -31,7 +40,4 @@ def search():
     # lists all files that contain the search term
     print("%d documents found" % res['hits']['total'])
     for doc in res['hits']['hits']:
-            print("%s" % (doc['_id']))
-
-
-
+        print("%s" % (doc['_id']))
